356edad8502718d8f4d3594967ed5ea3cbd58eed
Anthony <achen3741@gmail.com>
2021-03-14 22:24:34 -0400

    Added TabView

---
+++ Ultimate Recipes/Views/RecipeTabView.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeTabView.swift
+//  Ultimate Recipes
+//
+//  Created by Anthony Chen on 2021-03-14.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
